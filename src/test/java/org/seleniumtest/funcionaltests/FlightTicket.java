62985041fb54e7c3bfbb0d84483edcf4645edd85
Patryk Prentki <patryk.prentki90@gmail.com>
2024-08-20 18:25:03 +0200

    Added test dummyFlightTicketFutureDate

--- src/test/java/org/seleniumtest/funcionaltests/FlightTicket.java
+++ src/test/java/org/seleniumtest/funcionaltests/FlightTicket.java
@@ -40,5 +40,38 @@ public class FlightTicket {
                 .forEach(webElement -> webElement.click());
 
         driver.findElement(By.id("flight_oneway_btn")).click();
-        }
     }
+
+
+    @Test
+    public void dummyFlightTicketFutureDate() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://dummy-tickets.com/");
+        driver.findElement(By.name("source[]")).sendKeys("Po");
+        List<WebElement> source = driver.findElements(By.xpath("//ul[@class='suggestions-list']//p"));
+        source.stream().filter(webElement -> webElement.getText().contains("Al Ain"))
+                .forEach(webElement -> webElement.click());
+
+        driver.findElement(By.name("destination[]")).sendKeys("Ol");
+        List<WebElement> destination = driver.findElements(By.xpath("//input[@name='destination[]']/following-sibling::ul//p"));
+        destination.stream().filter(webElement -> webElement.getText().contains("Bole"))
+                .forEach(webElement -> webElement.click());
+
+        driver.findElement(By.name("departure[]")).click();
+        WebElement monthElement = driver.findElement(By.className("ui-datepicker-month"));
+        Select month = new Select(monthElement);
+        month.selectByIndex(10);
+
+        WebElement yearElement = driver.findElement(By.className("ui-datepicker-year"));
+        Select year = new Select(yearElement);
+        year.selectByValue("2025");
+
+        List<WebElement> days = driver.findElements(By.xpath("//table//td/a"));
+        days.stream().filter(webElement -> webElement.getText().equals("25"))
+                .forEach(webElement -> webElement.click());
+
+        driver.findElement(By.id("flight_oneway_btn")).click();
+
+
+    }
+}
