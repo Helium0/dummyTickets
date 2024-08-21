d24ae3f223e6673b4ead80102075a45460d478ba
Patryk Prentki <patryk.prentki90@gmail.com>
2024-08-21 17:33:52 +0200

    Added new test for book hotel in past date

--- src/test/java/org/seleniumtest/funcionaltests/HotelTicket.java
+++ src/test/java/org/seleniumtest/funcionaltests/HotelTicket.java
@@ -72,5 +72,60 @@ public class HotelTicket {
         }
     }
 
+    @Test
 
+    public void dummyHotelTicketPastDate() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://dummy-tickets.com/");
+        driver.findElement(By.xpath("//a[text()='Hotel']")).click();
+        WebElement city = driver.findElement(By.name("city[]"));
+        city.sendKeys("Londo");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//ul[@class='suggestions-cities-list']//p")));
+        List<WebElement> cities = driver.findElements(By.xpath("//ul[@class='suggestions-cities-list']//p"));
+        cities.stream().filter(webElement -> webElement.getText().contains("Londonderr"))
+                .forEach(webElement -> webElement.click());
+        driver.findElement(By.name("checkin[]")).click();
+
+        while (true) {
+            String actualYear = driver.findElement(By.className("ui-datepicker-year")).getText();
+            driver.findElement(By.xpath("//span[text()='Prev']")).click();
+            if (actualYear.equals("2022")) {
+                break;
+            }
+        }
+
+        WebElement datepickerMonth = driver.findElement(By.className("ui-datepicker-month"));
+        Select month = new Select(datepickerMonth);
+        month.selectByVisibleText("Dec");
+
+        List<WebElement> daysList = driver.findElements(By.xpath("//tbody//td/a"));
+        for (WebElement day : daysList) {
+            if (day.getText().equals("15")) {
+                day.click();
+                break;
+            }
+        }
+
+        driver.findElement(By.name("checkout[]")).click();
+        while (true) {
+            String actualYear = driver.findElement(By.className("ui-datepicker-year")).getText();
+            driver.findElement(By.xpath("//span[text()='Prev']")).click();
+            if (actualYear.equals("2023")) {
+                break;
+            }
+        }
+
+        datepickerMonth = driver.findElement(By.className("ui-datepicker-month"));
+        Select month2 = new Select(datepickerMonth);
+        month2.selectByVisibleText("Jun");
+
+        daysList = driver.findElements(By.xpath("//tbody//td/a"));
+        for (WebElement day : daysList) {
+            if (day.getText().equals("9")) {
+                day.click();
+                break;
+            }
+        }
+    }
 }
