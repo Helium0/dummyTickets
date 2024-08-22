03d33275acccafbb1dc2e7609d71e2a27e334fcb
Patryk Prentki <patryk.prentki90@gmail.com>
2024-08-22 12:30:58 +0200

    Created waitMethod

--- src/test/java/org/seleniumtest/BaseTest.java
+++ src/test/java/org/seleniumtest/BaseTest.java
@@ -4,22 +4,38 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
+import java.time.Duration;
 import java.util.List;
 
-public class BaseBrowser {
+public class BaseTest {
 
     protected static WebDriver driver;
 
-    public static WebDriver getDriver() {
-        if(driver == null) {
+
+    @BeforeMethod
+    public static WebDriver  before() {
             driver = new ChromeDriver();
             driver.get("https://dummy-tickets.com/");
             driver.manage().window().maximize();
-        }
+
+
         return driver;
     }
+
+    @AfterMethod
+    public static void after(){
+        driver.quit();
+        System.out.println("Test passed");
+    }
+
 
     public void datePicker(int monthh, String year, String day) {
         driver.findElement(By.name("departure[]")).click();
@@ -37,4 +53,11 @@ public class BaseBrowser {
 
     }
 
+    public void waitMethod(String xpath) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+    }
+
 }
+
+
