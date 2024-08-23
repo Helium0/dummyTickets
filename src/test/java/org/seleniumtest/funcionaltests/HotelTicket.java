01c8d3bc956846764adbaf6fdc3e806b3eec39f3
Patryk Prentki <patryk.prentki90@gmail.com>
2024-08-23 15:20:46 +0200

    Cleaned code and implemented methods from HomePage class

--- src/test/java/org/seleniumtest/funcionaltests/HotelTicket.java
+++ src/test/java/org/seleniumtest/funcionaltests/HotelTicket.java
@@ -1,43 +1,43 @@
 package org.seleniumtest.funcionaltests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.seleniumtest.BaseTest;
+import org.seleniumtest.pages.HomePage;
+import org.seleniumtest.tests.BaseTest;
 import org.testng.annotations.Test;
+
 
 
 
 public class HotelTicket extends BaseTest {
 
+    String hotelName = "Londonderr";
+    String xpath = "//ul[@class='suggestions-cities-list']//p";
 
     @Test
-    public void dummyHotelTicketFutureDate() {
-        driver.findElement(By.xpath("//a[text()='Hotel']")).click();
-        WebElement city = driver.findElement(By.name("city[]"));
-        city.sendKeys("Londo");
-        waitMethod("//ul[@class='suggestions-cities-list']//p");
-        searchHotelFromDynamicListMethod();
-        driver.findElement(By.name("checkin[]")).click();
-        hotelDatePickerMethod("2026", "Apr", "26","//span[text()='Next']");
-
-        driver.findElement(By.name("checkout[]")).click();
-        hotelDatePickerMethod("2027","Apr", "26","//span[text()='Next']");
+    public void dummyHotelTicketFutureDate()  {
+        HomePage homePage = new HomePage(driver);
+        homePage.pickCityHotel("Londo");
+        homePage.waitMethod(xpath);
+        homePage.searchHotelFromDynamicListMethod(hotelName);
+        homePage.clickActionOnTheElement();
+        homePage.waiMethod2();
+        homePage.hotelDatePickerMethod("2026", "Apr", "26","//span[text()='Next']");
+        homePage.hotelCheckout();
+        homePage.hotelDatePickerMethod("2027","Apr", "26","//span[text()='Next']");
 
     }
 
     @Test
 
     public void dummyHotelTicketPastDate() {
-        driver.findElement(By.xpath("//a[text()='Hotel']")).click();
-        WebElement city = driver.findElement(By.name("city[]"));
-        city.sendKeys("Londo");
-        waitMethod("//ul[@class='suggestions-cities-list']//p");
-        searchHotelFromDynamicListMethod();
-        driver.findElement(By.name("checkin[]")).click();
-        hotelDatePickerMethod("2022", "Dec","15","//span[text()='Prev']");
-
-        driver.findElement(By.name("checkout[]")).click();
-        hotelDatePickerMethod("2023", "Jun","9","//span[text()='Prev']");
+        HomePage homePage = new HomePage(driver);
+        homePage.pickCityHotel("Londo");
+        homePage.waitMethod(xpath);
+        homePage.searchHotelFromDynamicListMethod(hotelName);
+        homePage.clickActionOnTheElement();
+        homePage.waiMethod2();
+        homePage.hotelDatePickerMethod("2022", "Dec","15","//span[text()='Prev']");
+        homePage.hotelCheckout();
+        homePage.hotelDatePickerMethod("2023", "Jun","9","//span[text()='Prev']");
 
     }
 }
