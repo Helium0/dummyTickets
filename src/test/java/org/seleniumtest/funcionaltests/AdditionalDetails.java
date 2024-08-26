3ac82aca9ad65114323e381e5891453d4e680730
Patryk Prentki <patryk.prentki90@gmail.com>
2024-08-27 01:47:21 +0200

    Working on another Test formDetailsAndAddPassenger

--- src/test/java/org/seleniumtest/funcionaltests/AdditionalDetails.java
+++ src/test/java/org/seleniumtest/funcionaltests/AdditionalDetails.java
@@ -1,5 +1,6 @@
 package org.seleniumtest.funcionaltests;
 
+import org.openqa.selenium.By;
 import org.seleniumtest.pages.AdditionalDetailsPage;
 import org.seleniumtest.tests.BaseTest;
 import org.testng.annotations.Test;
@@ -11,6 +12,7 @@ public class AdditionalDetails extends BaseTest {
     String countryName = "Poland";
     String mobileNumber = "666-666-666";
     String userEmail = "automateTester@gmail.com";
+    String title = "Mr";
     String userName = "Patryk";
     String userSurname = "Tester";
     String userNationality = "Poland";
@@ -27,12 +29,31 @@ public class AdditionalDetails extends BaseTest {
         additionalDetailsPage.selectAndClickOnCorrectCountryCode(countryName);
         additionalDetailsPage.sendContactNumber(mobileNumber);
         additionalDetailsPage.sendUserEmail(userEmail);
-        additionalDetailsPage.chooseUserTitleAndClick();
+        additionalDetailsPage.chooseUserTitleAndClick(title);
         additionalDetailsPage.sendUserNameAndSurname(userName,userSurname);
         additionalDetailsPage.userDateOfBirthClickField();
         additionalDetailsPage.datePicker(month,year,day);
         additionalDetailsPage.sendNationality(userNationality);
-        additionalDetailsPage.chooseNationalityFromListAndClickAndClickNext();
+        additionalDetailsPage.chooseNationalityFromListAndClick();
+    }
+
+    @Test
+    public void formDetailsAndAddPassenger() throws InterruptedException {
+        AdditionalDetailsPage additionalDetailsPage = new AdditionalDetailsPage(driver);
+        additionalDetailsPage.orderFlyTicket();
+        additionalDetailsPage.waitForIdElement("select2-dialcodes-container");
+        additionalDetailsPage.selectAndTypeCountryCode();
+        additionalDetailsPage.selectAndClickOnCorrectCountryCode(countryName);
+        additionalDetailsPage.sendContactNumber(mobileNumber);
+        additionalDetailsPage.sendUserEmail(userEmail);
+        additionalDetailsPage.chooseUserTitleAndClick(title);
+        additionalDetailsPage.sendUserNameAndSurname(userName,userSurname);
+        additionalDetailsPage.userDateOfBirthClickField();
+        additionalDetailsPage.datePicker(month,year,day);
+        additionalDetailsPage.sendNationality(userNationality);
+        additionalDetailsPage.chooseNationalityFromListAndClick();
+        additionalDetailsPage.clickOnNextButton();
+
     }
 }
 
