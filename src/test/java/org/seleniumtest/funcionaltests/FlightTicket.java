4966fec5520d779fdbf43aae3071a500a8823c19
Patryk Prentki <patryk.prentki90@gmail.com>
2024-08-26 22:53:41 +0200

    Changed methods names for being more understandable

--- src/test/java/org/seleniumtest/funcionaltests/FlightTicket.java
+++ src/test/java/org/seleniumtest/funcionaltests/FlightTicket.java
@@ -1,5 +1,6 @@
 package org.seleniumtest.funcionaltests;
 
+import org.openqa.selenium.By;
 import org.seleniumtest.pages.HomePage;
 import org.seleniumtest.tests.BaseTest;
 import org.testng.annotations.Test;
@@ -14,11 +15,12 @@ public class FlightTicket extends BaseTest {
     public void oneWayTicketFutureDate() {
         HomePage homePage = new HomePage(driver);
         homePage.fromWhereWeGo("Po");
-        homePage.waitMethod("//ul[@class='suggestions-list']//p");
+        homePage.waitMethodForXpath("//ul[@class='suggestions-list']//p");
         homePage.airPortPicker(airPortDeparture);
         homePage.whereWeGo("Ol");
-        homePage.waitMethod("//input[@name='destination[]']/following-sibling::ul//p");
+        homePage.waitMethodForXpath("//input[@name='destination[]']/following-sibling::ul//p");
         homePage.airPortPicker(airPortDestination);
+        homePage.setPlaneDeparture();
         homePage.datePicker(10,"2025","26");
         homePage.flightOneWayButton();
 
@@ -28,13 +30,15 @@ public class FlightTicket extends BaseTest {
     public void oneWayTicketOldDate() {
         HomePage homePage = new HomePage(driver);
         homePage.fromWhereWeGo("Po");
-        homePage.waitMethod("//ul[@class='suggestions-list']//p");
+        homePage.waitMethodForXpath("//ul[@class='suggestions-list']//p");
         homePage.airPortPicker(airPortDeparture);
         homePage.whereWeGo("Ol");
-        homePage.waitMethod("//input[@name='destination[]']/following-sibling::ul//p");
+        homePage.waitMethodForXpath("//input[@name='destination[]']/following-sibling::ul//p");
         homePage.airPortPicker(airPortDestination);
+        homePage.setPlaneDeparture();
         homePage.datePicker(2,"2023", "10");
         homePage.flightOneWayButton();
 
     }
+
 }
