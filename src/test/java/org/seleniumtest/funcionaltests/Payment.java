74a2b351df217225ab53401008f2ed1d9eea623b
Patryk Prentki <patryk.prentki90@gmail.com>
2024-08-30 01:05:53 +0200

    New classes PaymentPage and Payment

---
+++ src/test/java/org/seleniumtest/funcionaltests/Payment.java
@@ -0,0 +1,4 @@
+package org.seleniumtest.funcionaltests;
+
+public class Payment {
+}
---
+++ src/test/java/org/seleniumtest/pages/PaymentPage.java
@@ -0,0 +1,8 @@
+package org.seleniumtest.pages;
+
+import org.seleniumtest.tests.BaseTest;
+
+public class PaymentPage extends BaseTest {
+
+
+}
