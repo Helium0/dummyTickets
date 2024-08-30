9af52069addd8d304c708dda69b3a0ac3446e338
Patryk Prentki <patryk.prentki90@gmail.com>
2024-08-30 11:07:36 +0200

    Working on another Test

--- src/test/java/org/seleniumtest/funcionaltests/Payment.java
+++ src/test/java/org/seleniumtest/funcionaltests/Payment.java
@@ -1,4 +1,24 @@
 package org.seleniumtest.funcionaltests;
 
-public class Payment {
+
+import org.seleniumtest.pages.PaymentPage;
+import org.seleniumtest.tests.BaseTest;
+import org.testng.annotations.Test;
+
+
+
+public class Payment extends BaseTest {
+
+    @Test
+    public void paymentThroughCreditCardDebit() throws InterruptedException {
+        PaymentPage paymentPage = new PaymentPage(driver);
+        FlightTicket flightTicket = new FlightTicket();
+        AdditionalDetails additionalDetails = new AdditionalDetails();
+        flightTicket.oneWayTicketFutureDate();
+        additionalDetails.formDetails();
+        paymentPage.flightTicket().oneWayTicketFutureDate();
+        paymentPage.additionalDetails().formDetails();
+        paymentPage.formDetails();
+        Thread.sleep(5000);
+    }
 }
--- src/test/java/org/seleniumtest/pages/PaymentPage.java
+++ src/test/java/org/seleniumtest/pages/PaymentPage.java
@@ -1,8 +1,59 @@
 package org.seleniumtest.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.seleniumtest.funcionaltests.AdditionalDetails;
+import org.seleniumtest.funcionaltests.FlightTicket;
 import org.seleniumtest.tests.BaseTest;
 
-public class PaymentPage extends BaseTest {
+public class PaymentPage  {
 
+    protected WebDriver driver;
+    String countryCode = "select2-dialcodes-container";
+    String countryName = "Poland";
+    String mobileNumber = "666-666-666";
+    String userEmail = "automateTester@gmail.com";
+    String title = "Mr";
+    String userName = "Patryk";
+    String userSurname = "Tester";
+    String userNationality = "Poland";
+    int month = 3;
+    String year = "2024";
+    String day = "12";
+    String purposeText = "Visa Extension";
+    String anotherPurposeText = "Proof of Return";
+    String textToSend = "THIS IS MY TEST !ąć99";
 
+    public PaymentPage(WebDriver driver){
+        PageFactory.initElements(driver,this);
+        this.driver = driver;
+    }
+
+    public FlightTicket flightTicket() {
+        return new FlightTicket();
+    }
+    public AdditionalDetails additionalDetails() {
+        return new AdditionalDetails();
+
+    }
+    public void formDetails() {
+        AdditionalDetailsPage additionalDetailsPage = new AdditionalDetailsPage(driver);
+        additionalDetailsPage.flightTicket().oneWayTicketFutureDate();
+        additionalDetailsPage.waitForCountryCodeIdElement(countryCode);
+        additionalDetailsPage.selectAndTypeCountryCode(countryName);
+        additionalDetailsPage.sendContactNumber(mobileNumber);
+        additionalDetailsPage.sendUserEmail(userEmail);
+        additionalDetailsPage.chooseUserTitleAndClick(title);
+        additionalDetailsPage.sendUserNameAndSurname(userName, userSurname);
+        additionalDetailsPage.userDateOfBirthClickField();
+        additionalDetailsPage.datePicker(month, year, day);
+        additionalDetailsPage.sendNationality(userNationality);
+        additionalDetailsPage.chooseNationalityFromListAndClick();
+        additionalDetailsPage.clickOnNextButton();
+        additionalDetailsPage.selectReceiveNow();
+        additionalDetailsPage.clickOnPurposeToBuyDummyTickets();
+        additionalDetailsPage.selectRightPurposeFromDropDown(purposeText);
+        additionalDetailsPage.sendText(textToSend);
+        additionalDetailsPage.clickOnNextButtonTwo();
+    }
 }
