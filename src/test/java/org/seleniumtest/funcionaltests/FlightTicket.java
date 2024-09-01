83a0d7081fa76db72172a04ee70d04c835264ce6
Patryk Prentki <patryk.prentki90@gmail.com>
2024-09-01 21:26:32 +0200

    Created variables and slightly modified methods

--- src/test/java/org/seleniumtest/funcionaltests/FlightTicket.java
+++ src/test/java/org/seleniumtest/funcionaltests/FlightTicket.java
@@ -1,27 +1,30 @@
 package org.seleniumtest.funcionaltests;
 
-import org.openqa.selenium.By;
+
 import org.seleniumtest.pages.HomePage;
 import org.seleniumtest.tests.BaseTest;
 import org.testng.annotations.Test;
 
 
+
 public class FlightTicket extends BaseTest {
 
-    String airPortDeparture = "Al Ain";
-    String airPortDestination = "Olkhovka";
+    private String airPortDeparture = "Al Ain";
+    private String airPortDestination = "Olkhovka";
+    private String parameterForWaitMethodForXpath = "//ul[@class='suggestions-list']//p";
+    private String parameterForWaitMethodForXpathDestination = "//input[@name='destination[]']/following-sibling::ul//p";
 
     @Test
     public void oneWayTicketFutureDate() {
         HomePage homePage = new HomePage(driver);
-        homePage.fromWhereWeGo("Po");
-        homePage.waitMethodForXpath("//ul[@class='suggestions-list']//p");
+        homePage.fromWhereWeGo("Po"); // you can change airportDeparture
+        homePage.waitMethodForXpath(parameterForWaitMethodForXpath);
         homePage.airPortPicker(airPortDeparture);
-        homePage.whereWeGo("Ol");
-        homePage.waitMethodForXpath("//input[@name='destination[]']/following-sibling::ul//p");
+        homePage.whereWeGo("Ol"); // you can change airportDestination
+        homePage.waitMethodForXpath(parameterForWaitMethodForXpathDestination);
         homePage.airPortPicker(airPortDestination);
         homePage.setPlaneDeparture();
-        homePage.datePicker(10,"2025","26");
+        homePage.datePicker(10,"2025","26"); // you can change dates
         homePage.flightOneWayButton();
 
     }
@@ -29,14 +32,14 @@ public class FlightTicket extends BaseTest {
     @Test
     public void oneWayTicketOldDate() {
         HomePage homePage = new HomePage(driver);
-        homePage.fromWhereWeGo("Po");
-        homePage.waitMethodForXpath("//ul[@class='suggestions-list']//p");
+        homePage.fromWhereWeGo("Po");  // you can change airportDeparture
+        homePage.waitMethodForXpath(parameterForWaitMethodForXpath);
         homePage.airPortPicker(airPortDeparture);
-        homePage.whereWeGo("Ol");
-        homePage.waitMethodForXpath("//input[@name='destination[]']/following-sibling::ul//p");
+        homePage.whereWeGo("Ol");  // you can change airportDestination
+        homePage.waitMethodForXpath(parameterForWaitMethodForXpathDestination);
         homePage.airPortPicker(airPortDestination);
         homePage.setPlaneDeparture();
-        homePage.datePicker(2,"2023", "10");
+        homePage.datePicker(2,"2023", "10"); // you can change dates
         homePage.flightOneWayButton();
 
     }
