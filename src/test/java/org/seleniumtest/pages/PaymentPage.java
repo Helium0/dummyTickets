44cac5770ae66c7699ec4a66838003c9cf70cd14
Patryk Prentki <patryk.prentki90@gmail.com>
2024-09-01 21:07:51 +0200

    Added locators and methods in to PaymentPage class

--- src/test/java/org/seleniumtest/pages/PaymentPage.java
+++ src/test/java/org/seleniumtest/pages/PaymentPage.java
@@ -1,59 +1,79 @@
 package org.seleniumtest.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 import org.seleniumtest.funcionaltests.AdditionalDetails;
-import org.seleniumtest.funcionaltests.FlightTicket;
-import org.seleniumtest.tests.BaseTest;
+
+
+import java.util.List;
+
 
 public class PaymentPage  {
 
     protected WebDriver driver;
-    String countryCode = "select2-dialcodes-container";
-    String countryName = "Poland";
-    String mobileNumber = "666-666-666";
-    String userEmail = "automateTester@gmail.com";
-    String title = "Mr";
-    String userName = "Patryk";
-    String userSurname = "Tester";
-    String userNationality = "Poland";
-    int month = 3;
-    String year = "2024";
-    String day = "12";
-    String purposeText = "Visa Extension";
-    String anotherPurposeText = "Proof of Return";
-    String textToSend = "THIS IS MY TEST !ąć99";
+
+    @FindBy(name = "state")
+    private List<WebElement> stateList;
+
+    @FindBy(name = "state")
+    private WebElement state;
+
+    @FindBy(name = "submit")
+    private WebElement submitClick;
+
+    @FindBy(xpath = "(//button[@name='submit'])[3]")
+    private WebElement submitXpathButton;
+
+    @FindBy(xpath = "//input[@value='bank']")
+    private WebElement bankRadiobutton;
+
+    @FindBy(xpath = "//label[@class='swicthbutton']")
+    private WebElement changeCurrencyButton;
+
+    @FindBy(xpath = "//input[@value='bank_usd']")
+    private WebElement usdBank;
 
     public PaymentPage(WebDriver driver){
         PageFactory.initElements(driver,this);
         this.driver = driver;
     }
 
-    public FlightTicket flightTicket() {
-        return new FlightTicket();
-    }
     public AdditionalDetails additionalDetails() {
         return new AdditionalDetails();
 
     }
-    public void formDetails() {
-        AdditionalDetailsPage additionalDetailsPage = new AdditionalDetailsPage(driver);
-        additionalDetailsPage.flightTicket().oneWayTicketFutureDate();
-        additionalDetailsPage.waitForCountryCodeIdElement(countryCode);
-        additionalDetailsPage.selectAndTypeCountryCode(countryName);
-        additionalDetailsPage.sendContactNumber(mobileNumber);
-        additionalDetailsPage.sendUserEmail(userEmail);
-        additionalDetailsPage.chooseUserTitleAndClick(title);
-        additionalDetailsPage.sendUserNameAndSurname(userName, userSurname);
-        additionalDetailsPage.userDateOfBirthClickField();
-        additionalDetailsPage.datePicker(month, year, day);
-        additionalDetailsPage.sendNationality(userNationality);
-        additionalDetailsPage.chooseNationalityFromListAndClick();
-        additionalDetailsPage.clickOnNextButton();
-        additionalDetailsPage.selectReceiveNow();
-        additionalDetailsPage.clickOnPurposeToBuyDummyTickets();
-        additionalDetailsPage.selectRightPurposeFromDropDown(purposeText);
-        additionalDetailsPage.sendText(textToSend);
-        additionalDetailsPage.clickOnNextButtonTwo();
+
+    public Select select() {
+        return new Select(state);
     }
+
+    public void chooseState(String myState) {
+        select().getOptions().stream()
+                .filter(element -> element.getText().equals(myState))
+                .forEach(element -> element.click());
+    }
+
+    public void submitButton() {
+        submitClick.click();
+    }
+
+    public void bankButton() {
+        bankRadiobutton.click();
+    }
+
+    public void changeCurrency() {
+        changeCurrencyButton.click();
+    }
+
+    public void chooseUSDBank() {
+        usdBank.click();
+    }
+
+    public void submitXpathButton() {
+        submitXpathButton.click();
+    }
+
 }
